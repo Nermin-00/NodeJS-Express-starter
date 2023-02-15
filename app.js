75b3925a44bd8ac737bde70f1e878f77381f55b8
Nermin-00 <71044724+Nermin-00@users.noreply.github.com>
2023-02-15 23:50:46 +0100

    Updated comments.

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 // __dirname - path to curr. dir.
 // __filename - file name
 // require - function to use modules(CommonJS)
-// module - info about current module  (file)
-// process - inf. ab. env. where the program is being exeec.
+//module - info about current module (file)
+//process - inf. ab. env. where the program is being exeec.
 
 //  console.log(module)
 
 //CommonJS, every file is a module
-//Modules - Encapsulated Code (only share minimum)
+//Modules - Encapsulated Code(only share minimum)
 
 //In order to access  variables from 4-names.js we use require.
 //We can assign it or require it.
